c1deafe4a4c47b8d8044683f41750d7f66f40cf4
halida <linjunhalida@gmail.com>
2013-06-08 13:47:50 +0800

    finish ex6

--- mlclass-ex6/dataset3Params.m
+++ mlclass-ex6/dataset3Params.m
@@ -22,8 +22,22 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+% current c: 1.000000, sigma: 0.100000, error: 0.030000
 
+error = 10000
 
+for ci = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30],
+    for si = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30],
+        model = svmTrain(X, y, ci, @(x1, x2) gaussianKernel(x1, x2, si));
+        predictions = svmPredict(model, Xval);
+        en = mean(double(predictions ~= yval));
+        if (en < error),
+           C = ci; sigma = si; error = en;
+        end
+        fprintf("c: %f, sigma: %f, error: %f\n", ci, si, en);
+        fprintf("current c: %f, sigma: %f, error: %f\n", C, sigma, error);
+    end
+end
 
 
 
